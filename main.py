7a78f477ee58febc3ae9ecaf36cc924c148a6517
Yehor Shamanskyi <sudormrf777@gmail.com>
2022-10-06 02:06:24 +0300

    flex path, new apps

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import json
 
 programs = json.load(open('programs.json'))
 print(programs)
-
+downloads = os.path.join(os.environ['USERPROFILE'], "Downloads")
+os.chdir(downloads)
 for name, program in programs.items():
     r = httpx.get(program['url'], follow_redirects=True)
     open(program['filename'], 'wb').write(r.content)
     if program['filename'].endswith(".zip"):
-        with zipfile.ZipFile(f"C:\\Users\\KremenTech01\\PycharmProjects\\Project_reload\\{program['filename']}", 'r') as zip_ref:
-            zip_ref.extractall("C:\\Users\KremenTech01\\PycharmProjects\\Project_reload\\")
+        with zipfile.ZipFile(f"{downloads}\\{program['filename']}", 'r') as zip_ref:
+            zip_ref.extractall(f"{downloads}")
 
 
     # os.system(program['command'])
